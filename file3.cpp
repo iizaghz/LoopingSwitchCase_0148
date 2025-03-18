8608530d4b76244aadc60eb08f36a29cffc4157e
iizaghz <sukmahawaiza@gmail.com>
2025-03-18 20:40:36 +0700

    menambahkan switch untuk menu

--- file3.cpp
+++ file3.cpp
@@ -20,3 +20,28 @@ int main (){
         cin >> pilihan;
 
         
+        switch(pilihan){
+            case 1 :
+            cout << "Menghitung Luas Persegi Panjang" << endl;
+            cout << "Masukkan Panjang :";
+            cin >> panjang;
+            cout << "Masukkan Lebar :";
+            cin >> lebar;
+            cout << "Luas Persegi Panjang = "
+            <<luasPersegiPanjang(panjang,lebar) << endl;
+            break;
+        case 2 :
+        cout << "Menghitung Luas Lingkaran" << endl;
+        cout << "Masukkan Jari - Jari = ";
+        cin >> jejari;
+        cout << "Luas Lingkaran = "
+        <<luasLingkaran(jejari) << endl;
+        break;
+    case 3 :
+        break;
+    default :
+            cout << "Pilihan anda salah" << endl;
+            break;
+        }
+    }while(pilihan !=3);
+}
